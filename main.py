3b1e8662ccb56e5136dff7efb3cb090bb942592d
Khushi <kapoorjitender011jk@gmail.com>
2026-06-22 20:42:16 +0530

    added while loop

--- main.py
+++ main.py
@@ -9,13 +9,15 @@ print("--> To update record enter U")
 print("--> To delete record enter D")
 print("--> To display student with highest marks enter H")
 print("--> To exit enter E\n")
-ui= input("Enter your input: ").upper()
-if ui == "A":
-    id= int(input("enter student id: "))
-    std_name= input("enter student name: ")
-    std_age= int(input("enter student age: "))
-    std_marks= int(input("enter student marks: "))
-    record = (id,std_name,std_age,std_marks)
-    obj.student_info(record)
-if ui=="V":
-    print(obj.a)
+
+while True:
+    ui= input("What you want to do today?: ").upper()
+    if ui == "A":
+        id= int(input("enter student id: "))
+        std_name= input("enter student name: ")
+        std_age= int(input("enter student age: "))
+        std_marks= int(input("enter student marks: "))
+        record = (id,std_name,std_age,std_marks)
+        obj.student_info(record)
+    if ui=="V":
+        print(obj.a)
